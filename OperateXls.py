87e5ab00e6313dc60f5b1d41634640f306b3c0a5
hispaniora <hispaniora@yahoo.com>
2020-07-16 16:59:50 +0800

    branch02's first commit after new 'OperateXls.py'

---
+++ OperateXls.py
@@ -0,0 +1,9 @@
+import sys
+import xlrd
+
+
+# 获取Excel第一个sheet中的数据
+xlsBook = xlrd.open_workbook(r'F:\testData\GDAPIs1.xls')  # 以只读的方式打开xlsx文件
+xlsSheet = xlsBook.sheets()[0]
+rowCount = xlsSheet.nrows  # 获取sheet的行数
+print(str(rowCount))
