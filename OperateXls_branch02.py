fda379d344ed3c5df8c2d94480002f86e903be6b
hispaniora <hispaniora@yahoo.com>
2020-07-17 09:54:48 +0800

    edit content

--- OperateXls_branch02.py
+++ OperateXls_branch02.py
@@ -2,8 +2,7 @@ import sys
 import xlrd
 
 
-# 获取Excel第一个sheet中的数据,并输出行数。
 xlsBook = xlrd.open_workbook(r'F:\testData\GDAPIs1.xls')  # 以只读的方式打开xlsx文件
-xlsSheet = xlsBook.sheets()[0]
+xlsSheet = xlsBook.sheets()[0]  # 获取Excel第一个sheet中的数据
 rowCount = xlsSheet.nrows  # 获取sheet的行数
 print(str(rowCount))
